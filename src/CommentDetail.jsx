9bed95e5c9e6d2c09eadb77c2963238df96b41ae
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-07 02:21:55 +0700

    Add CommentDetail component

---
+++ src/CommentDetail.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default ({ name, avatar, date, text}) => (
+  <div className="comment">
+    <a href="/" className="avatar">
+      <img src={avatar} alt="avatar" />
+    </a>
+    <div className="content">
+      <a href="/" className="author">
+        {name}
+      </a>
+      <div className="metadata">
+        <span className="date">{date}</span>
+      </div>
+      <div className="text">{text}</div>
+    </div>
+  </div>
+)
--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 // Import the React and ReactDOM libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CommentDetail from './CommentDetail'
+
+const comments = [
+  {
+    name: 'Sam',
+    avatar: 'https://picsum.photos/60/60',
+    date: 'Today at 6:00 PM',
+    text: 'Nice blog post!',
+  },
+  {
+    name: 'Sarah',
+    avatar: 'https://picsum.photos/60/60',
+    date: 'Today at 6:00 PM',
+    text: 'Nice blog post!',
+  },
+]
 
 // Create a react component
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src="https://picsum.photos/60/60" alt="avatar"/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00 PM</span>
-          </div>
-          <div className="text">
-            Nice blog post!
-          </div>
-        </div>
-      </div>
+      {comments.map((comment, index) => <CommentDetail key={index} {...comment} />)}
     </div>
   )
 }
