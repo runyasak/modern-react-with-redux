35907d904f192ec10d8d63d93bb265ea429358f4
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-07 15:53:24 +0700

    Add getDisplayText function

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -8,13 +8,16 @@ const getSeason = (lat, month) => {
   return lat > 0 ? 'winter' : 'summer';
 };
 
+const getDisplayText = (season) => (season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach');
+
 export default ({ lat }) => {
   const season = getSeason(lat, new Date().getMonth());
+
   return (
     <div>
       Seasion Display
       {' '}
-      {season}
+      {getDisplayText(season)}
     </div>
   );
 };
