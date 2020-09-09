f8eacfd5a4d7352296d8c8e1ebe0980be103f6ba
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-10 00:12:49 +0700

    Add onSubmit to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => (
-  <div className="ui container" style={{ padding: '16px' }}>
-    <SearchBar />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchResult: '' };
+  }
+
+  onSearchSubmit = (searchResult) => {
+    this.setState({ searchResult });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ padding: '16px' }}>
+        Search result: {this.state.searchResult}
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
