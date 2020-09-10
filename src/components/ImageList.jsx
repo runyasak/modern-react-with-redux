bb3789d5c19498e19a51b98c09e4b80b574b79c0
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-10 18:55:28 +0700

    Add ImageList.css

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import './ImageList.css';
 
-const ImageList = ({ images }) => images.map(({ description, id, urls: { regular } }) => (
-  <img key={id} src={regular} alt={description} width="100%" />
-));
+const ImageList = ({ images }) => {
+  const imageHtmls = images.map(({ description, id, urls: { regular } }) => (
+    <img key={id} src={regular} alt={description} />
+  ));
+
+  return <div className="image-list">{imageHtmls}</div>;
+};
 
 export default ImageList;
