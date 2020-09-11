afaac43258cdbd6e0f3e8493be1ebb56b54c5480
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-11 17:33:53 +0700

    Add renderedItems to Accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,19 +1,24 @@
 import React, { Fragment } from 'react';
 
-const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => (
-    <Fragment key={item.title}>
-      <div className="title active">
-        <i className="dropdown icon" />
-        {item.title}
-      </div>
-      <div className="content active">
-        <p>{item.content}</p>
-      </div>
-    </Fragment>
-  ));
-
-  return <div className="ui styled accordion">{renderedItems}</div>;
+const onTitleClick = (index) => {
+  console.log('Title Clicked', index);
 };
+
+const renderedItems = (items) => items.map((item, index) => (
+  <Fragment key={item.title}>
+    <div
+      className="title active"
+      onClick={() => onTitleClick(index)}
+    >
+      <i className="dropdown icon" />
+      {item.title}
+    </div>
+    <div className="content active">
+      <p>{item.content}</p>
+    </div>
+  </Fragment>
+));
+
+const Accordion = ({ items }) => <div className="ui styled accordion">{renderedItems(items)}</div>;
 
 export default Accordion;
