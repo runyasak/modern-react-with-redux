cc67655b0d119b55e8e29d1154a9c83394c0e1d0
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-14 12:46:31 +0700

    Implement searchWiki with renderedResults on Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { searchWiki } from '../providers/wikipedia';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const searchResults = await searchWiki(term);
+
+      setResults(searchResults.query.search);
+    };
+
+    search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => (
+    <div key={result.pageid} className="item">
+      <div className="content">
+        <div className="header">
+          {result.title}
+        </div>
+        {result.snippet}
+      </div>
+    </div>
+  ));
 
   return (
-    <div>
+    <div className="search">
       <div className="ui form">
         <div className="field">
           <label htmlFor="search">
@@ -18,6 +41,9 @@ const Search = () => {
             />
           </label>
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   );
