af1e181c679fc0b74f8406982d3d8ef8944a1018
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-14 12:58:52 +0700

    Add Go link and empty term condition

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,11 +12,23 @@ const Search = () => {
       setResults(searchResults.query.search);
     };
 
-    search();
+    if (term) {
+      search();
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => (
     <div key={result.pageid} className="item">
+      <div className="right floated content">
+        <a
+          className="ui button"
+          href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          rel="noreferrer"
+          target="_blank"
+        >
+          Go
+        </a>
+      </div>
       <div className="content">
         <div className="header">
           {result.title}
