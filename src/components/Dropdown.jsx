799ec4a7751a564b59b697dff9ac64cb59e4bcce
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-09-14 18:36:51 +0700

    Add open with visible styles to Dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
   const renderedOptions = options
     .filter((option) => option.value !== selected.value)
     .map((option) => (
@@ -14,16 +16,22 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       </div>
     ));
 
+  const visibleActiveClasses = open ? 'visible active' : '';
+  const visibleTransitionClasses = open ? 'visible transition' : '';
+
   return (
     <div className="ui form">
       <div className="field">
         <label htmlFor="dropdown" className="label">
           Select a Color
         </label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${visibleActiveClasses}`}
+        >
           <i className="dropdown icon" />
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">
+          <div className={`menu ${visibleTransitionClasses}`}>
             {renderedOptions}
           </div>
         </div>
